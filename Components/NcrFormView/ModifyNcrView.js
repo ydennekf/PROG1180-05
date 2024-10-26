3a8b385291ca00ab979345a89adf9d763085f6cb
ZachT <zachztl1@gmail.com>
2024-10-26 17:38:05 -0400

    Updated form

--- Components/NcrFormView/ModifyNcrView.js
+++ Components/NcrFormView/ModifyNcrView.js
@@ -19,8 +19,16 @@ export function ModifyNcrView(targetID, employee, report=null){ // HANDLES BOTH 
             app.storage.print()
         }
     // If a report is passed an additional toolbar should be added with functionality like closing the report etc.
-    function validateForm(e) {
-        e.preventDefault();
+    function validateForm() {
+        //e.preventDefault();
+        //e.stopPropagation()
+        // console.log(e.currentTarget.id)
+        // if(e.currentTarget.id === "btn-cancel"){
+        //     ReportList('root', app.employee, reportData)
+        //     app.history.flush()
+        //     console.log("Cancelling and redirecting to home page.")
+        // }
+
         console.log(report)
         const formData = validateQualityAssuranceForm(report !== null, report); // if there is a report given we're not updating the report
        // For now it's just the QA portion being validated
@@ -58,9 +66,17 @@ export function ModifyNcrView(targetID, employee, report=null){ // HANDLES BOTH 
     document.getElementById('create-report-btn').style.display = 'none';
     document.getElementById('report-search').style.display = 'none';
     document.getElementById(targetID).innerHTML = html;
-    document.getElementById('ncr-create-form').addEventListener('submit', (e)=>{validateForm(e); console.log("awesome")});
+    document.getElementById('ncr-create-form').addEventListener('submit', (e)=>{e.preventDefault()});
+    document.getElementById('btn-submit-ncr').addEventListener('click', (e)=>{validateForm(e)})
+    document.getElementById('btn-cancel').addEventListener('click', cancelEdit)
+
     trapFocus(document.getElementById(targetID));
 
 }
 
 
+function cancelEdit(){
+    ReportList('root', app.employee, reportData)
+    app.history.flush()
+    console.log("Cancelling and redirecting to home page.")
+}

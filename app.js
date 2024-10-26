4f2fa54bb5373496e067a3acbba97d414e69e4b9
ZachT <zachztl1@gmail.com>
2024-10-25 23:55:53 -0400

    Updated footer and accessibility

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ import { employees } from './Data/employeeData.js';
 import { reportData } from './Data/new_reportData.js';
 
 // import utility functions
-import { resetForm } from './Components/utils/utils.js';
+import { append, insert, resetForm } from './Components/utils/utils.js';
 import { initApp } from './AppState.js';
 import { ModifyNcrView } from './Components/NcrFormView/ModifyNcrView.js';
 import { app } from './AppState.js';
+import { AccessibilityPanel } from './Components/AccesibilityPanel.js';
 
 
 let RedirectToIndex = (employee, reports) => {
@@ -42,7 +43,7 @@ let handleLogin = (event) => {
         if (employeeData.password == passwordInput) {
             // success
             RedirectToIndex(employeeData, reportData)
-           
+           AccessibilityPanel()
             // fire methods to display the index view of the NCR reports.
         }
         else {

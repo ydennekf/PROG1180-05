e52d9c660fafa7459d5095671e73b9c50de4b9e7
jeni <jenniejeongg@gmail.com>
2024-11-21 23:09:05 -0500

    CSS for login box location and placeholder upper

--- Components/Login.js
+++ Components/Login.js
@@ -5,39 +5,31 @@ export let  createLogin = (targetID, onLogin) => {
     
     let login =
         `
-            
-
-
-        <form id="loginForm" action="#" method="post" aria-label="loginForm"  class="login-form">
-            <div class="col-1 input-effect">
-                
-
-                <div class="input-wrapper">
-                    <input type="text" id="username" name="username" required aria-required="true" tabindex="2" class="effect-1"/>
-                    <label for="username">Username</label>
-                    <span class="focus-border"></span>
-
-                </div>
-
-                <div class="input-wrapper">
-                    
-                    <input type="password" id="password" name="password" required aria-required="true" tabindex="2" class="effect-1"/>
-                    <label for="password">Password</label>
-                    <span class="focus-border"></span>
-
-                </div>
-                <div class="remember-me">
+        <div class="login-container">
+            <form id="loginForm" action="#" method="post" aria-label="loginForm"  class="login-form">
+                <div class="col-1 input-effect">
+                    <div class="input-wrapper">
+                        <input type="text" id="username" name="username" required aria-required="true" tabindex="2" class="effect-1"/>
+                        <label for="username">Username</label>
+                        <span class="focus-border"></span>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="password" id="password" name="password" required aria-required="true" tabindex="2" class="effect-1"/>
+                        <label for="password">Password</label>
+                        <span class="focus-border"></span>
+                    </div>
+                    <div class="remember-me">
                         <input type="checkbox" id="rememberMe">
                         <label for="rememberMe">Remember Me</label>
                     </div>
-                
-          </div>
-            <div class="login-btn-container">
+                </div>
+                <div class="login-btn-container">
                     <button type="submit" class="login-btn">LOG IN</button>
-            </div>  
-        </form>
-        <div class="forgot-password">
-            <a href="#">Forgot Password?</a>
+                </div>  
+            </form>
+            <div class="forgot-password">
+                <a href="#">Forgot Password?</a>
+            </div>
         </div>`;
     document.getElementById(targetID).className.replace()
     document.getElementById(targetID).innerHTML = login;

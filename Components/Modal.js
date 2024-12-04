38d72088fb34e1d6fce96691610b76bdc1e63406
fraser kennedy <ydennekrf@gmail.com>
2024-12-03 23:50:55 -0500

    added various style changes, added close button to modal

--- Components/Modal.js
+++ Components/Modal.js
@@ -1,8 +1,10 @@
 import { trapFocus } from "./utils/utils.js";
+import * as SVG from "./svgs.js";
 
 export let createModal = (targetID, title, message, autoCloseDelay) => {
     const modal = `
-    <div id="modal" role="dialog" aria-labelledby="modalTitle" aria-hidden="true" style="display: block;">
+    <div id="modal" role="dialog" class="notify-modal" aria-labelledby="modalTitle" aria-hidden="true">
+    <div id="closeModal" class="icon">${SVG.minusSVG()}</div>
         <div>
             <h2 id="modalTitle">${title}</h2>
             <p>${message}</p>
@@ -15,26 +17,38 @@ export let createModal = (targetID, title, message, autoCloseDelay) => {
             setTimeout(() => {
                 
                     document.getElementById('errorPanel').innerHTML = '';
-                    document.getElementById("loginForm").reset();
+                    if(document.getElementById('loginForm')){
+                        document.getElementById("loginForm").reset();
+                    }
+
                 
             }, delay)
  
     }
 
-    if(autoCloseDelay){
-        ResetErrorPanel(autoCloseDelay);
-    }
+
 
     document.getElementById(targetID).innerHTML = modal;
     let component = document.getElementById(targetID);
     trapFocus(component);
 
     document.getElementById('closeModal').addEventListener('click', () => {  document.getElementById('errorPanel').innerHTML = '';
-        document.getElementById("loginForm").reset(); });
+
+    if(document.getElementById('loginForm')){
+          document.getElementById("loginForm").reset();
+    }
+      });
+
     document.getElementById('closeModal').addEventListener('keydown', (event) => {
         if(event.key === 'Escape' || (event.key === 'Enter' && document.activeElement === document.getElementById('closeModal'))){
             document.getElementById('errorPanel').innerHTML = '';
-            document.getElementById("loginForm").reset();
+            if(document.getElementById("loginForm")){
+                 document.getElementById("loginForm").reset();
+            }
+
         }
     })
+    if(autoCloseDelay){
+        ResetErrorPanel(autoCloseDelay);
+    }
 }
